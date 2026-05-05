d25d66566ed0408e055d4e8c47a73c99fb02efbb
Yutakkkkj <natan.m@escolar.ifrn.edu.br>
2026-05-05 17:32:19 -0300

    ff

--- motores3pjd3v2026/Assets/script/MenuController.cs
+++ motores3pjd3v2026/Assets/script/MenuController.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+
     public void StartGame()
     {
-        GameManager.Instance.LoadScene("GetStarted_Scene");
         GameManager.Instance.ChangeState(GameManager.GameState.Gameplay);
+        GameManager.Instance.LoadScene("GetStarted_Scene");
     }
 
+    // Sai do jogo
     public void QuitGame()
     {
+        Debug.Log("Saindo do jogo...");
         Application.Quit();
     }
 }
